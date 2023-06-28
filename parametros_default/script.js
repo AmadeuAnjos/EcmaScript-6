c5c0d1653e89b7d2e7fbaa77d48ade054bddbd30
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-06-27 21:08:42 -0300

    parametros default

---
+++ parametros_default/script.js
@@ -0,0 +1,11 @@
+function contarUmaHistoria(
+    personagem = "Fubá",
+    atividade = "Correr",
+    nome_dono = "João"
+  ) {
+    document.write(
+      `Era uma vez um cachorro chamado ${personagem}, ele adorava ${atividade}, seu dono era o ${nome_dono} e eles viveram felizes para sempre`
+    );
+  }
+  
+  contarUmaHistoria("Repolho", "Enterrar ossos", "Pedro");
